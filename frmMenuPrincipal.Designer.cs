fd619a02f6d8463a548a6c6db236e5a7800f59fe
Andrés Bolaños <andresbol1706@gmail.com>
2024-10-31 18:07:32 -0600

    Configuración inicial de formulario del menu principal

--- frmMenuPrincipal.Designer.cs
+++ frmMenuPrincipal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BikerStriker
 {
-    partial class Form1
+    partial class frmMenuPrincipal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 379);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "frmMenuPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menú Principal";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
